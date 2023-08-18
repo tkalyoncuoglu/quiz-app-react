d39ca3c19964af5d4ababc41e038fe3eb9125a82
29534411212 <tkalyoncuoglu@gmail.com>
2023-08-18 22:01:19 +0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<></>);
